4c584a5df40a3f4bb6628e487a1ae4e9df9cb70d
fedesuarez16 <fedesuarez16@gmail.com>
2024-07-12 02:57:49 -0300

    idk

--- src/app/(pages)/checkout/metadata.ts
+++
@@ -1,9 +0,0 @@
-// src/app/(pages)/checkout/metadata.ts
-export const metadata: Metadata = {
-    title: 'Account',
-    description: 'Create an account or log in to your existing account.',
-    openGraph: mergeOpenGraph({
-      // tus datos de openGraph
-    }),
-  };
-  
